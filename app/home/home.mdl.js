1f115974e89a2e6baa1652182e24ed9d18d2a00b
Todd B. Adams <todd.adams@orwellg>
2016-02-12 13:58:18 +0000

    theme

---
+++ app/home/home.mdl.js
@@ -0,0 +1,48 @@
+﻿(function () {
+    'use strict';
+    var
+		TEMPLATE_PATH = 'app/home/',
+		DATA_PATH = 'app/home/',
+		ROUTES = [{
+		    name: 'home',
+		    state: {
+		        url: '/',
+		        templateUrl: TEMPLATE_PATH + 'home.html',
+		        controller: 'wtHome',
+		        controllerAs: "vm"
+		    }
+		}];
+
+    angular.module('wt.home', ['ngMaterial', 'wt.routes'])
+		.constant('wtHomeConfig', {
+		    routes: ROUTES,
+		    dataPath: DATA_PATH
+		})
+		.config(moduleConfig)
+		.controller('wtHome', HomeController);
+
+
+    /**
+	 * Module configuration
+	 */
+    moduleConfig.$inject = ['wtRouteProvider', 'wtHomeConfig'];
+
+    function moduleConfig(wtRoutes, moduleConfig) {
+        wtRoutes.$get().setRoutes(moduleConfig.routes);
+    }
+    /**
+	 * Home controller
+	 */
+    HomeController.$inject = [];
+
+    function HomeController() {
+        var vm = this;
+
+       
+        // controller activation
+        (function () {
+            
+        })();
+    }
+
+})();
